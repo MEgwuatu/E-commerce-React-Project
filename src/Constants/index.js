735b1416128592604a80c6be765fefb8ebad8ee3
egwuatu modestus <egwuatumodestus@gmail.com>
2025-04-08 09:25:00 +0100

    Updated 8/04/2025

---
+++ src/Constants/index.js
@@ -0,0 +1,23 @@
+//navbar links
+export const navlinks = [
+    {
+        name: 'Home',
+       title: 'Home',
+       url: '/'
+        
+    },
+    {
+        name: 'About',
+        title: 'About',
+        url: '/about'
+    },
+    {
+        name: 'Product',
+        title: 'Product',
+        url: '/product'
+    },
+    {
+        name: 'Contact',
+        title: 'Contact'
+    },
+]
